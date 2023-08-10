c0c4233a206ef8c4ea38eff3d00b188a9f0313dd
joaogabrieldonato <jgsd.2001@gmail.com>
2023-08-10 20:21:38 -0300

    Create panel definitions

---
+++ include/definitions.h
@@ -0,0 +1,18 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <TM1637Display.h>
+#include <TM1637TinyDisplay6.h>
+#include <U8g2lib.h>
+#include <hard_defs.h>
+
+//Four digits display
+TM1637Display Four(Fourdigits_CLK, Fourdigits_DIO);
+
+//Six digits display
+TM1637TinyDisplay6 Six(Sixdigits_CLK, Sixdigits_DIO);
+
+//OLED
+U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0,SCLK_OLED, SDA_OLED, U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
+
+#endif
