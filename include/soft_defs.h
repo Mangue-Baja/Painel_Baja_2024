794e1c7381cb0ee1dfbabf97cdfc2c92151066c7
joaogabrieldonato <jgsd.2001@gmail.com>
2023-12-14 12:54:19 -0300

    Remove useless variables

--- include/soft_defs.h
+++ include/soft_defs.h
@@ -1,58 +1,52 @@
 #ifndef SOFT_DEFS_H
 #define SOFT_DEFS_H
 
-#ifdef SOFT_DEFS_H
-  #include <Arduino.h>
-  #include "hard_defs.h"
-#endif
+/*=============================*/
+      #include "hard_defs.h"
+/*=============================*/
 
 typedef struct {
-  uint16_t segundos = 0;
-  uint8_t minutos = 0;
-  uint8_t horas = 0;
+  uint16_t seconds = 0;
+  uint8_t minutes = 0;
+  uint8_t hours = 0;
 
-  unsigned long time_current=0;
-  unsigned long tempo_volta_ms = 0;
-} Tempo;
+  unsigned long time_current = 0;
+  unsigned long time_lap_ms = 0;
+} Time;
 
-Tempo Enduro;
-Tempo penultima_volta;
-Tempo ultima_volta;
-Tempo Volta;
-Tempo Delta;
+Time Endurance;
+Time Lap;
+//Tempo penultima_volta;
+//Tempo ultima_volta;
+//Tempo Delta;
 
 typedef struct {
-  uint8_t Pin;
-  int buttonState;                     // The current reading from the input pin
-  int lastButtonState = LOW;           // Tthe previous reading from the input pin
+  uint8_t Pin = SWITCH;
+  uint8_t buttonState;                 // The current reading from the input pin
+  uint8_t lastButtonState = LOW;       // Tthe previous reading from the input pin
   unsigned long lastDebounceTime = 0;  // The last time the output pin was toggled
-  uint8_t mode = 0;
-} Perifericos;
+  //uint8_t mode = 0;
+} Peripherals;
 
-Perifericos Switch;
-Perifericos Button;
+Peripherals Switch;
 
 //Struct to receive all panel data
 //if you want to add data to the panel, change it not only in the variable but also in the front ECU
 typedef struct {
-  uint16_t velocidade;
-  uint16_t rpm;
-  uint16_t temp_motor;
-  uint16_t combustivel;
-  uint16_t battery;
-  uint16_t temp_cvt;
-  uint16_t telemetry;
+  uint16_t speed;     // 2by
+  uint16_t rpm;       // 2by
+  uint8_t temp_motor; // 1by
+  uint16_t level;     // 2by
+  uint8_t battery;    // 1by
+  uint8_t temp_cvt;   // 1by
+  uint8_t sot;        // 1by
 } Txtmng;
-
-bool emergency_led_state;
-bool boolean1HZ;
-bool boolean5HZ;
 
 unsigned long lastDebounceTime = 0;
 const int debounceDelay = 200;          //Time in milliseconds for button debounce
 
-typedef enum PROGMEM {TEMPO_DE_ENDURO, CRONOMETRO, DELTA_CRONOMETRO} tempo_six_digits;
+typedef enum PROGMEM {ENDURANCE_TIMER, STOPWATCH, DELTA_CRONOMETRO} tempo_six_digits;
 
-byte six_digits_state = TEMPO_DE_ENDURO;
+byte six_digits_state = ENDURANCE_TIMER;
 
 #endif
